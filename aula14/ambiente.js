fc7685719a0a66c0bb6135ec207d6c02a2e0fe8a
Anatrizz1 <cerq1205ana@gmail.com>
2025-09-15 10:59:49 -0300

    criação de dois sites para executar o JS

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+/*let cont = 0;
+while (cont <= 10) {
+  console.log(`Contador: ${cont}`);
+    cont += 1;
+}
+console.log('Fim do loop');
+
+let cont = 0;
+do {
+  console.log(`Contador: ${cont}`);
+  cont += 1;
+} while (cont <= 10);
+console.log('Fim do loop');
+*/
+console.log('Início do loop');
+for (let cont = 0; cont <= 10; cont ++){
+  console.log(`Contador: ${cont}`);
+}   
+console.log('Fim do loop');
